2ac90e853ec5f0522be9400259896e4f272bf3f0
RivanFadlani <rivanfadlani7@gmail.com>
2024-10-22 10:56:50 +0700

    mengkonfigurasi tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
+            gridTemplateColumns: {
+                "custom-grid": "repeat(3, minmax(100px, 1fr))",
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Manrope", defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#242F9B",
+                gradient: "#646FD4",
+                dark: "#111827",
             },
         },
     },
